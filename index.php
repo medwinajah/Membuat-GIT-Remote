71641bf594061e72396c53a8b3ea0701ad9e4711
medwinajah <medwinalfredo6@gmail.com>
2026-02-24 20:13:38 +0700

    Menambahkan list pada file index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Belajar GIT Remote</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam ex laudantium maiores praesentium debitis, itaque optio impedit eum explicabo dicta aspernatur velit! Mollitia temporibus, cum tenetur quos fugit eius non!</p>
+    <ul>
+        <?php $i=1; ?>
+        <li><?= $i; ?></li>
+        <?php $i++; ?>
+    </ul>
 </body>
 </html>
